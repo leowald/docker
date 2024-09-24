d0daed9849df54872932bb776f458c8040caf992
leowald <121869480+leowald@users.noreply.github.com>
2024-09-24 15:34:25 +0300

    Update index.js

--- node/index.js
+++ node/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 3000;
+const port = 80;
 
 const accounts = JSON.parse(fs.readFileSync("accounts.json"));
 console.log(accounts);
